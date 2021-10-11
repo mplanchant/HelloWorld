f031bfd47c283adf8217207895f5fb0430e17163
Matthew Planchant <matt@logiccache.co.uk>
2021-10-11 15:30:07 +0100

    Initial commit.

---
+++ src/main/kotlin/com/example/HelloWorld.kt
@@ -0,0 +1,55 @@
+package com.example
+
+import com.example.formats.JacksonMessage
+import com.example.formats.jacksonMessageLens
+import com.example.routes.ExampleContractRoute
+import org.http4k.contract.bind
+import org.http4k.contract.contract
+import org.http4k.contract.openapi.ApiInfo
+import org.http4k.contract.openapi.v3.OpenApi3
+import org.http4k.contract.security.ApiKeySecurity
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method.GET
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.core.then
+import org.http4k.core.with
+import org.http4k.filter.DebuggingFilters.PrintRequest
+import org.http4k.format.Jackson
+import org.http4k.lens.Query
+import org.http4k.lens.int
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+
+val app: HttpHandler = routes(
+    "/ping" bind GET to {
+        Response(OK).body("pong")
+    },
+
+    "/formats/json/jackson" bind GET to {
+        Response(OK).with(jacksonMessageLens of JacksonMessage("Barry", "Hello there!"))
+    },
+
+    "/contract/api/v1" bind contract {
+        renderer = OpenApi3(ApiInfo("HelloWorld API", "v1.0"), Jackson)
+    
+        // Return Swagger API definition under /contract/api/v1/swagger.json
+        descriptionPath = "/swagger.json"
+    
+        // You can use security filter tio protect routes
+        security = ApiKeySecurity(Query.int().required("api"), { it == 42 }) // Allow only requests with &api=42
+    
+        // Add contract routes
+        routes += ExampleContractRoute()
+    }
+)
+
+fun main() {
+    val printingApp: HttpHandler = PrintRequest().then(app)
+
+    val server = printingApp.asServer(Jetty(9000)).start()
+
+    println("Server started on " + server.port())
+}
